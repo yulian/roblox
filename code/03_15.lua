dc189f7617a21e092a3da357e7ff58be6a7f2f01
yulian <yulian@naver.com>
2023-01-14 06:40:21 +0900

    Create 03_15.lua

---
+++ code/03_15.lua
@@ -0,0 +1,17 @@
+local str1 = "Lua"
+local str2 = 'Programming'
+
+-- 문자열출력
+print(str1)
+
+-- 큰따옴표와 작은따옴표
+print("\"큰따옴표\"")
+print("\'작은따옴표\'")
+
+-- 문자열결합
+print(str1, str2)
+
+--[[
+여러줄주석
+표기법
+]]--
