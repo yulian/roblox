10c3b833a5cb45459b275154b0b509c55de2bd50
yulian <yulian@naver.com>
2023-01-14 06:36:58 +0900

    Create 03_12.lua

---
+++ code/03_12.lua
@@ -0,0 +1,13 @@
+local score = 85
+
+if score >= 90 then
+  print("A")
+elseif score >= 80 then
+  print("B")
+elseif score >= 70 then
+  print("C")
+elseif score >= 60 then
+  print("D")
+else
+  print("F")
+end
