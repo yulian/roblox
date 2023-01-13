7a01d7811f4dfd1b4bb140d898a301f831d540c1
yulian <yulian@naver.com>
2023-01-14 06:15:37 +0900

    Create 05_08.lua

---
+++ code/05_08.lua
@@ -0,0 +1,21 @@
+local jumpUpItem = script.Parent
+local boostPower = 100
+
+local function changeJump(otherPart)
+    local partParent = otherPart.Parent
+    local humanoid = partParent:FindFirstChild("Humanoid")
+    if humanoid then
+        jumpUpItem:Destroy()
+        local currentJump = humanoid.JumpPower
+        humanoid.UseJumpPower = true
+        if(humanoid.JumpPower<boostPower) then
+            humanoid.JumpPower = boostPower
+            print("점프력향상")
+        end
+        wait(3)
+        humanoid.JumpPower = currentJump
+        print("점프력복구")
+    end
+end
+
+jumpUpItem.Touched:Connect(changeJump)
