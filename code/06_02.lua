02ac2251a983c99cb0edb7dd1f5f59854a38e33e
yulian <yulian@naver.com>
2023-01-14 06:01:19 +0900

    Update 06_02.lua

--- code/06_02.lua
+++ code/06_02.lua
@@ -1,2 +1,13 @@
+local DataStoreService = game:GetService("DataStoreService")
+local coinStore = DataStoreService:GetDataStore("PlayerCoin")
+
+-- 사용자ID 가져오기
 local player = game.Players.PlayerAdded:Wait()
-print(player.UserId)
+
+-- 데이터 가져오기
+local getSuccess, currentGold = pcall(function()
+    return coinStore:GetAsync(player.UserId)
+end)
+if getSuccess then
+    print(currentGold)
+end
