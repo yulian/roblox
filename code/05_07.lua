91d9ac570b15374dc9bc7e350902f9c14bf068ad
yulian <yulian@naver.com>
2023-01-14 06:14:47 +0900

    Create 05_07.lua

---
+++ code/05_07.lua
@@ -0,0 +1,18 @@
+local speedUpItem = script.Parent
+local originalSpeed = nil
+
+local function changeSpeed(otherPart)
+    local partParent = otherPart.Parent
+    local humanoid = partParent:FindFirstChild("Humanoid")
+    if humanoid then
+        originalSpeed = humanoid.WalkSpeed
+        humanoid.WalkSpeed = 100
+        print("속도향상")
+        speedUpItem:Destroy()
+        wait(3)
+        humanoid.WalkSpeed = originalSpeed
+        print("속도회복")
+    end
+end
+
+speedUpItem.Touched:Connect(changeSpeed)
